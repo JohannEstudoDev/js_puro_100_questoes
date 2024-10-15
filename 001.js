21d11487e420d14dc2f8fe9de9130a994f50ad26
johann <johann@MacBook-Air-de-johann.local>
2024-10-15 15:27:05 -0300

    commit exercicio 001

---
+++ 001.js
@@ -0,0 +1,18 @@
+
+var nome = "johann"
+const cpf = "000.000.000-00"
+
+function mostraSobrenome(){
+    let sobreNome = "torres";
+
+    return sobreNome
+}
+
+console.log(nome)
+console.log(mostraSobrenome())
+console.log(cpf)
+console.log(nome, " ", mostraSobrenome() , " ", cpf)
+
+
+
+
