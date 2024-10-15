1da2da6ca1360a99c81ccf06a7967e95a32b2dbb
johann <johann@MacBook-Air-de-johann.local>
2024-10-15 15:35:33 -0300

    exercicio 002

---
+++ 002.js
@@ -0,0 +1,13 @@
+var produto = "mouse";
+const codigoProduto = "001";
+
+
+function exibirDetalhesProdutos(){
+    let categoria = "perifericos";
+
+    return categoria
+}
+
+console.log(produto)
+console.log(exibirDetalhesProdutos())
+console.log(codigoProduto)
