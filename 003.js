b62946a3e3e4644f59f9a63be69e521fa4780277
johann <johann@MacBook-Air-de-johann.local>
2024-10-15 16:47:13 -0300

    exercicio 003

---
+++ 003.js
@@ -0,0 +1,20 @@
+var nomeAluno = "johann";
+const matricula = "001";
+
+function infoAluno(tecnologia){
+    let curso = "Computação"
+
+    return (`O aluno: ${nomeAluno} de matricula ${matricula} cursa ${curso}`)
+}
+console.log(infoAluno())
+
+
+
+
+
+
+
+
+
+
+
