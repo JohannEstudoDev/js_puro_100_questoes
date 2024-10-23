2f24806f4bfb1c288b88fddd58cade165f2cb619
johann <johann@MacBook-Air-de-johann.local>
2024-10-23 10:32:50 -0300

    adicao exercicio 006

---
+++ 006.js
@@ -0,0 +1,23 @@
+/*6 - Crie um script que contenha um objeto literal chamado pessoa. 
+O objeto deve armazenar informações sobre uma pessoa, incluindo:
+nome: O nome da pessoa.
+cpf: O CPF da pessoa.
+cidade: A cidade onde a pessoa mora. Declare e inicialize o objeto pessoa com valores para nome, cpf e cidade. 
+Imprima cada uma das propriedades do objeto pessoa usando console.log.
+*/
+
+
+const Pessoa = {
+    nome: "Johann",
+    cpf: "000.000.000-00",
+    cidade: "Cabedelo"
+}
+
+console.log("Nome: ",Pessoa.nome)
+
+console.log("CPF: ",Pessoa.cpf)
+
+console.log ("Cidade: ",Pessoa.cidade)
+
+
+
