75c7c497a6f5904ee165e67b4fe73e8a5c71af8a
johann <johann@MacBook-Air-de-johann.local>
2024-10-25 08:08:22 -0300

    adicao exercicio 017

---
+++ 017.js
@@ -0,0 +1,14 @@
+/* 
+17 - Crie um script que declare um objeto chamado livro com as propriedades titulo, 
+autor e anoPublicacao. Inicialize o objeto com valores e imprima todas as propriedades usando console.log.
+*/
+
+const Livro = {
+    titulo: "Java",
+    autor: "Kelson",
+    anoPublicacao: "2024"
+}
+console.log(Livro.titulo)
+console.log(Livro.autor)
+console.log(Livro.anoPublicacao)
+
