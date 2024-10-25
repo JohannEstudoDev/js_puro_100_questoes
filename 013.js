e5343cc738b69f9070a51bb9fd9c5a809595f1e7
johann <johann@MacBook-Air-de-johann.local>
2024-10-24 22:01:36 -0300

    adicao exercicio 013

---
+++ 013.js
@@ -0,0 +1,15 @@
+/* 
+13 - Crie um script que declare uma variável var largura e atribua a ela um valor numérico. 
+Declare uma função que calcule a largura ao cubo e imprima o resultado.
+*/
+
+const numero = 10
+
+function calculaAoCubo (numero){
+
+    let valorAoCubo = numero * numero * numero 
+    
+    return valorAoCubo
+}
+console.log(calculaAoCubo(numero))
+
