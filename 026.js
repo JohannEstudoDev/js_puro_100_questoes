0763b746d310e2dca9a36729527e1ca10feb4ee6
johann <johann@MacBook-Air-de-johann.local>
2024-10-25 12:43:44 -0300

    adicao exercicio 026

---
+++ 026.js
@@ -0,0 +1,20 @@
+/*
+26 - Crie um script que declare uma função que receba um array de strings como parâmetro e 
+retorne um novo array onde cada string esteja em letras minúsculas. 
+Utilize a função toLowerCase() para converter as strings.
+*/
+
+const palavras = ["Java", "KOTLIN", "PYthoN"]
+
+function convertePalavrasMinusculas(palavras){
+
+    const palavrasMinusculas = []
+
+    for (let i = 0; i < palavras.length; i++){
+        palavrasMinusculas.push(palavras[i].toLowerCase())
+    }
+    return palavrasMinusculas
+
+}
+
+console.log(convertePalavrasMinusculas(palavras))
