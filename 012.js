c13113197613c8a8d6af052089e382725e11b61e
johann <johann@MacBook-Air-de-johann.local>
2024-10-24 21:55:13 -0300

    adicao exercicio 012

---
+++ 012.js
@@ -0,0 +1,13 @@
+/*
+12 - Crie um script que declare uma variável let altura e atribua a ela um valor numérico. 
+Em seguida, declare uma função que calcule a altura ao quadrado e imprima o resultado. 
+*/
+
+let numero = 2
+
+function calculaAltura (numero){
+    const calculo = numero * numero
+    return calculo
+}
+console.log(calculaAltura(numero))
+
