84fed533a675ef907092657d6bf61b3d532feeee
johann <johann@MacBook-Air-de-johann.local>
2024-10-25 11:50:18 -0300

    adicao exercicio 025

---
+++ 025.js
@@ -0,0 +1,27 @@
+/*
+25 - Crie um script que declare uma função que receba um array de strings como parâmetro e 
+retorne um novo array onde cada string esteja em letras maiúsculas. 
+Utilize a função toUpperCase() para converter as strings.
+*/
+
+const palavras = ["java", "kotlin", "python"]
+
+function palavrasMaiusculas(palavras){
+
+const mudaPalavrasParaMaiusculas = palavras.map(palavras => palavras.toUpperCase())
+
+    return mudaPalavrasParaMaiusculas
+
+}
+console.log(palavrasMaiusculas(palavras))
+
+/*
+const palavras = ["java", "kotlin", "python"];
+
+function palavrasMaiusculas(palavras) {
+  const palavrasMaiusculas = palavras.map(palavra => palavra.toUpperCase());
+  return palavrasMaiusculas;
+}
+
+console.log(palavrasMaiusculas(palavras));
+*/
