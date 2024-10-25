ea83e9d92b3de1ceb23d258177f6717b14428a03
johann <johann@MacBook-Air-de-johann.local>
2024-10-25 11:10:44 -0300

    adicao exercicio 022

---
+++ 022.js
@@ -0,0 +1,11 @@
+/*
+22 - Crie um script que declare um array de números e utilize a função map() para criar um novo 
+array onde cada número seja multiplicado por 2. Imprima o novo array.
+*/
+
+const numeros = [2,4,5,6]
+
+const dobraNumeros = numeros.map(numeros => numeros * 2)
+
+console.log(dobraNumeros)
+
