b19088e570636c25f1733122f51eba68de4009d1
johann <johann@MacBook-Air-de-johann.local>
2024-10-25 11:34:48 -0300

    adicao exercicio 024

---
+++ 024.js
@@ -0,0 +1,12 @@
+/*
+24 - Crie um script que declare um array de strings e utilize a função join() para concatenar 
+todas as strings em uma única string, separadas por vírgulas. Imprima o resultado.
+*/
+
+const palavras = ["java", "kotlin", "python"]
+
+const juntaPalavras = palavras.join(', ')
+
+console.log(juntaPalavras)
+
+
